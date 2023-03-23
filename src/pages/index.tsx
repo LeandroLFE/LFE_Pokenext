830642bc1907f1528161983d653bf35885de66b6
LeandroLFE <lfe.leandro@gmail.com>
2023-03-23 12:03:07 -0300

    fetch PokeApi

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,40 @@
+import { PokeAPI } from 'pokeapi-types';
+
 import styles from '@/styles/Home.module.css'
 
-export default function Home() {
+interface props {
+  pokemons: PokeAPI.Pokemon[],
+}
+
+
+export async function getStaticProps() {
+
+  const maxPokemons = 151;
+  const api = "https://pokeapi.co/api/v2/pokemon"
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  // Add pokemon index
+  data.results.forEach((item: PokeAPI.Pokemon, index: number) => {
+    item.id = index + 1;
+  })
+  return {
+    props: {
+      pokemons: data.results,
+    }
+  }
+}
+
+export default function Home({ pokemons }: props) {
   return (
-    <>
-      <main className={styles.main}>
-        <h1>LFE_Pokenext</h1>
-      </main>
-    </>
+    <div>
+      <h1>LFE_PokeNext</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
